808f9cf02cd3fe7bb0ac7dabd4d9ee60252dcef2
Chen Jiasen Jiasen <1005220@mymail.sutd.edu.sg>
2022-06-07 08:51:11 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,12 +1,9 @@
 class Movie < ActiveRecord::Base
-  def self.with_ratings(ratings_list)
-    # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
-    #  movies with those ratings
-    # if ratings_list is nil, retrieve ALL movies
-    if ratings_list.nil?
-      Movie.all
-    else
-      Movie.where(:rating => ratings_list)
-    end
+  def self.all_ratings
+    #Return ['G','PG','PG-13','R']
+    %w(G PG PG-13 R)
+  end
+  def self.filter_by_ratings(ratings)
+    Movie.where(rating: ratings)
   end
 end
